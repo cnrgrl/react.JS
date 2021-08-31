fe12d15a43b0af4f0dd6271b87938497844973a7
Caner-Ugurlu <ugrl.cnr@gmail.com>
2021-08-31 12:37:26 +0200

    start configs

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import React, { Component } from 'react';
+
+class Todo extends Component {
+    state = {  }
+    render() { 
+        return (  
+            <div>
+                <p>hello</p>
+            </div>
+        );
+    }
+}
+ 
+export default Todo;
+
